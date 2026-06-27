d674ca478c60ae8e56b5ad10ab1960dad7160b45
Rifah Arif <arifalilko2018@gmail.com>
2026-06-27 15:52:01 +0530

    Changes

--- next-backend/server.js
+++ next-backend/server.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve uploaded files
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api", allRoutes);
